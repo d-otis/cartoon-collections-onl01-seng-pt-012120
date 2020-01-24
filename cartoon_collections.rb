641f27ba5595fc9d11faa1a548c9ca7e919c82f7
Dan Foley <dan@dan-foley.com>
2020-01-24 03:58:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def find_the_cheese(coll)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  binding.pry
+  # binding.pry
   coll.find {|item| cheese_types.any?(item)}
   
 end
